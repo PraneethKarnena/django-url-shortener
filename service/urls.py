2d17a270f6309c5014b2f588bd0edd96d6782e02
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-13 01:41:12 +0530

    added a soft fail

--- service/urls.py
+++ service/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view),
     path('api/shorten-url/', views.url_shortener_api, name='url_shortener_api'),
-    path('api/shorten-url', views.url_shortener_api, name='url_shortener_api'),
+    path('api/shorten-url', views.url_shortener_api, name='url_shortener_api_safe'),
     path('l/<slug:slug>/', views.redirector_view),
 ]
